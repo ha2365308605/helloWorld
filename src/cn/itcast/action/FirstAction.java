999b66583748c575ca41e12bb9402a575dff1db2
PC <PC@DESKTOP-ISNAMCO>
2018-09-23 17:27:56 +0800

    这是我的第一个上传项目

---
+++ src/cn/itcast/action/FirstAction.java
@@ -0,0 +1,16 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FirstAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String execute() throws Exception{
+		return SUCCESS;
+		
+	}
+
+}
---
+++ src/cn/itcast/action/HelloWorldAction.java
@@ -0,0 +1,17 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String execute() throws Exception{
+		return SUCCESS;
+		
+	}
+
+}
